64a545824ae9472f99e709c898dd8203749cce43
Abhinav5383 <itsabhinav5383@gmail.com>
2026-02-03 10:10:20 +0530

    .,.,

--- .config/hypr/scripts/fix-speaker.sh
+++ .config/hypr/scripts/fix-speaker.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
-state=$(playerctl status 2>/dev/null)
+CARD_INFO=$(pactl list cards | awk '
+/^Card #/ { if (++c > 1) exit }
+{ print }
+')
 
-~/.config/hypr/scripts/reset-profile.sh
+CARD_NAME="$(printf '%s\n' "$CARD_INFO" | awk -F': ' '/^\s*Name:/ { print $2 }')"
 
-if [ "$state" = "Playing" ]; then
+ACTIVE_PROFILE="$(printf '%s\n' "$CARD_INFO" \
+    | awk -F': ' '/^\s*Active Profile:/ { print $2 }')"
+
+if [[ "$ACTIVE_PROFILE" == "off" ]]; then
+    echo "Warning: The current active profile is set to 'off'."
+    echo "Please select a valid profile through 'pavucontrol > Configuration' before running this script."
+    exit 1
+fi
+
+MEDIA_STATE=$(playerctl status 2>/dev/null)
+
+# need to toggle card-profile once to reset the audio stack properly
+pactl set-card-profile "$CARD_NAME" off
+pactl set-card-profile "$CARD_NAME" "$ACTIVE_PROFILE"
+
+if [ "$MEDIA_STATE" = "Playing" ]; then
   sleep 0.5
   playerctl play
 fi
---
+++ .config/hypr/scripts/logon.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+~/.config/hypr/scripts/unmute-mic.sh
+pw-play ~/.config/hypr/assets/win-11-startup.mp3
