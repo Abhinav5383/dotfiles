5f75818fabb2b6299eec3345e7ca78539474861a
Abhinav5383 <itsabhinav5383@gmail.com>
2026-04-11 12:47:48 +0530

    increase scrcpy bitrate

--- scripts/scrcpy.sh
+++ scripts/scrcpy.sh
@@ -1,5 +1,5 @@
 scrcpy \
     --video-codec=h265 \
     --audio-codec=opus \
-    --video-bit-rate=6M --max-fps=60 --audio-buffer=20 \
+    --video-bit-rate=12M --max-fps=60 --audio-buffer=20 \
     --keyboard=uhid
