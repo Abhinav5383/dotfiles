c7add65adad7895e684c022108b673d0a3203986
Abhinav5383 <itsabhinav5383@gmail.com>
2026-04-19 23:46:18 +0530

    fix quotes

--- scripts/screenrec.sh
+++ scripts/screenrec.sh
@@ -2,7 +2,7 @@
 
 killall -SIGINT -q gpu-screen-recorder && exit 0
 
-video="$HOME/Videos/captures/$(date +"%Y-%m-%d_%H-%M-%S.mp4")"
+video="$HOME/Videos/captures/$(date +'%Y-%m-%d_%H-%M-%S.mp4')"
 
 gpu-screen-recorder -w portal -a default_output -o "$video"
 notify-send -t 5000 -u low "GPU Screen Recorder" "Video saved to $video"
