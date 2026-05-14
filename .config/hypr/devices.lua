dcd1cd3616f59c17cc0863e6280c63f2478e0e9a
Abhinav5383 <itsabhinav5383@gmail.com>
2026-05-14 21:12:46 +0530

    adjust repeat_delay slightly

--- .config/hypr/devices.lua
+++ .config/hypr/devices.lua
@@ -15,7 +15,7 @@ hl.config({
     sensitivity = 1,
     accel_profile = "custom 0.1315929378 0.000 0.070 0.140 0.229 0.334 0.439 0.544 0.649 0.802 0.962 1.122 1.281 1.441 1.601 1.760 1.920 2.080 2.239 2.399 2.559 2.718 2.878 3.038 3.367",
     repeat_rate = 25,
-    repeat_delay = 200,
+    repeat_delay = 250,
 
     touchpad = {
       natural_scroll = true
